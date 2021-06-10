eb462bf520a4c6dd0cfb0167e3b9863cc62ce886
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 15:54:26 +0200

    exercice 4.6 fait

---
+++ exercice4.6.php
@@ -0,0 +1,21 @@
+<?php
+    $firstNumber = "Bilbo";
+    $secondNumber = "Baggins";
+    $age = 111;
+    function result($firstNumber,$secondNumber,$age) : string 
+    {
+        return "Bonjour .$firstNumber. .$secondNumber., tu as .$age. ans.";
+    }?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 4.6</title>
+</head>
+<body>
+    <h1>Exercice 6 Partie 4 : Les fonctions PHP</h1>
+    <p>Bonjour <?=$firstNumber?> <?=$secondNumber?>, tu as <?=$age?> ans.</p>  
+</body>
+</html>
